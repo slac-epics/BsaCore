5f75b54a01f67a313321a57f74f93bad349619c5
till straumann <till.straumann@alumni.tu-berlin.de>
2018-04-20 08:53:15 -0700

    Had forgotten to add BsaCheckAlarms.c...

---
+++ src/BsaCheckAlarms.c
@@ -0,0 +1,66 @@
+#include <BsaApi.h>
+#include <alarm.h>
+
+/* Borrowed from B. Dalesio's implementation (aiRecord) */
+void
+BsaCheckAlarms(double val, BsaAlarmLevels lvls, BsaStat *status, BsaSevr *severity)
+{
+    double  hyst, lalm;
+    double  alev;
+    BsaSevr asev;
+
+    hyst = lvls->hyst;
+    lalm = lvls->lalm;
+
+    /* alarm condition hihi */
+    asev = lvls->hhsv;
+    alev = lvls->hihi;
+    if (asev && (val >= alev || ((lalm == alev) && (val >= alev - hyst)))) {
+        if ( asev > *severity ) {
+			*severity  = asev;
+            *status    = HIHI_ALARM;
+            lvls->lalm = alev;
+		}
+        return;
+    }
+
+    /* alarm condition lolo */
+    asev = lvls->llsv;
+    alev = lvls->lolo;
+    if (asev && (val <= alev || ((lalm == alev) && (val <= alev + hyst)))) {
+        if ( asev > *severity ) {
+			*severity  = asev;
+            *status    = LOLO_ALARM;
+            lvls->lalm = alev;
+		}
+        return;
+    }
+
+    /* alarm condition high */
+    asev = lvls->hsv;
+    alev = lvls->high;
+    if (asev && (val >= alev || ((lalm == alev) && (val >= alev - hyst)))) {
+        if ( asev > *severity ) {
+			*severity  = asev;
+            *status    = HIGH_ALARM;
+            lvls->lalm = alev;
+		}
+        return;
+    }
+
+    /* alarm condition low */
+    asev = lvls->lsv;
+    alev = lvls->low;
+    if (asev && (val <= alev || ((lalm == alev) && (val <= alev + hyst)))) {
+        if ( asev > *severity ) {
+			*severity  = asev;
+            *status    = LOW_ALARM;
+            lvls->lalm = alev;
+		}
+        return;
+    }
+
+    /* we get here only if val is out of alarm by at least hyst */
+    lvls->lalm = val;
+    return;
+}
